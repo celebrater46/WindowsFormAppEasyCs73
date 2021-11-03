5ac643b1099e336fda60ec0d05bb6999e6ac7f80
hideru <jota.16.stigma.world@gmail.com>
2021-11-04 07:50:36 +0900

    Added CheckBox and Test succeeded

--- WindowsFormsAppEasyCs73/Form1.cs
+++ WindowsFormsAppEasyCs73/Form1.cs
@@ -12,8 +12,10 @@ namespace WindowsFormsAppEasyCs73
 {
     public partial class Form1 : Form
     {
-        private Label label;
+        private Label label, label2;
         private Button button;
+        private CheckBox cb1, cb2;
+        private FlowLayoutPanel flp;
 
         public void Display()
         {
@@ -25,26 +27,61 @@ namespace WindowsFormsAppEasyCs73
             InitializeComponent();
             this.Text = "Button Test Sample";
             this.Width = 250;
-            this.Height = 100;
+            this.Height = 250;
 
             label = new Label();
             label.Text = "Welcome.";
             label.Dock = DockStyle.Top;
+            
+            label2 = new Label();
+            label2.Text = "Choose your vehicle.";
+            label2.Dock = DockStyle.Top;
+            label2.Width = 200;
+
+            cb1 = new CheckBox();
+            cb2 = new CheckBox();
+
+            cb1.Text = "Car";
+            cb2.Text = "Train";
+
+            flp = new FlowLayoutPanel();
+            flp.Dock = DockStyle.Bottom;
+            flp.Parent = this;
+
+            label2.Parent = flp;
+            cb1.Parent = flp;
+            cb2.Parent = flp;
+
+            cb1.CheckedChanged += new EventHandler(CbCheckedChanged);
+            cb2.CheckedChanged += new EventHandler(CbCheckedChanged);
 
             button = new Button();
             button.Text = "Buy";
-            button.Dock = DockStyle.Bottom;
+            button.Dock = DockStyle.Top;
 
             button.Click += new EventHandler(ClickButton);
 
-            label.Parent = this;
             button.Parent = this;
+            label.Parent = this;
         }
 
         public void ClickButton(Object sender, EventArgs e)
         {
             label.Text = "Thank you for shopping.";
             button.Enabled = false;
+        }
+
+        public void CbCheckedChanged(Object sender, EventArgs e)
+        {
+            CheckBox temp = (CheckBox) sender;
+            if (temp.Checked == true)
+            {
+                label2.Text = string.Format("Checked {0}.", temp.Text);
+            }
+            else if (temp.Checked == false)
+            {
+                label2.Text = string.Format("Canceled {0}.", temp.Text);
+            }
         }
     }
 }
